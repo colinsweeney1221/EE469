9ad31fa9977550f06969ffece01a5e7675f7562b
colinsweeney1221 <colinsweeney1221@gmail.com>
2025-05-07 13:13:54 -0700

    control unit

--- lab3/control.sv
+++ lab3/control.sv
@@ -9,29 +9,203 @@ module control(opCode, reg2Loc, reg30, regWrite, ALUSrc, ALUOp, setFlags, memRea
 
     always_comb begin
         case (opCode)
-            12'h488:12'h489:   // ADDI
+            11'h488:11'h489: begin   // ADDI
+                             reg2Loc      = 1'bX
+                             reg30        = 1'b0
+                             regWrire     = 1'b1
+                             ALUSrc       = 1'b1
+                             ALUOp        = 3'b010
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'b0
+                             PC2Reg       = 1'b0
+                             branch       = 1'b0
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'b0
+                             end
 
-            12'h750:           // ADDS
+            11'h750:         begin  // ADDS
+                             reg2Loc      = 1'b0
+                             reg30        = 1'b0
+                             regWrite     = 1'b1
+                             ALUSrc       = 1'b0
+                             ALUOp        = 3'b010
+                             setFlags     = 1'b1
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'b0
+                             PC2Reg       = 1'b0
+                             branch       = 1'b0
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'bX
+                             reg2PC       = 1'b0
+                             extend       = 2'bX
+                             end
             
-            12'h0A0:12'h0BF:   // B
+            11'h0A0:11'h0BF: begin  // B
+                             reg2Loc      = 1'bX
+                             reg30        = 1'bX
+                             regWrite     = 1'b0
+                             ALUSrc       = 1'bX
+                             ALUOp        = 3'bX
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'bX
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b1
+                             reg2PC       = 1'b0
+                             extend       = 1'b01
+                             end
             
-            12'h2A0:12'h2A7:   // B.LT
+            11'h2A0:11'h2A7: begin  // B.LT
+                             reg2Loc      = 1'bX
+                             reg30        = 1'bX
+                             regWrite     = 1'b0
+                             ALUSrc       = 1'bX
+                             ALUOp        = 3'bX
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'b1
+                             whichCMP     = 1'b1
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'b10
+                             end
             
-            12'h2A0:12'h4BF:   // BL
+            11'h2A0:11'h4BF: begin  // BL
+                             reg2Loc      = 1'bX
+                             reg30        = 1'b1
+                             regWrite     = 1'b1
+                             ALUSrc       = 1'bX
+                             ALUOp        = 3'bX
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'b1
+                             branch       = 1'bX
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b1
+                             reg2PC       = 1'b0
+                             extend       = 2'b01
+                             end
             
-            12'h6B0:           // BR
+            11'h6B0:         begin  // BR
+                             reg2Loc      = 1'b1
+                             reg30        = 1'bX
+                             regWrite     = 1'b0
+                             ALUSrc       = 1'b0
+                             ALUOp        = 3'b000
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'bX
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'bX
+                             reg2PC       = 1'b1
+                             extend       = 2'bX
+                             end
             
-            12'h5A0:12'h5A7:   // CBZ
+            11'h5A0:11'h5A7: begin  // CBZ
+                             reg2Loc      = 1'b1
+                             reg30        = 1'bX
+                             regWrite     = 1'b0
+                             ALUSrc       = 1'b0
+                             ALUOp        = 3'b000
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'b1
+                             whichCMP     = 1'b0
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'b10
+                             end
             
-            12'h7C2:           // LDUR
+            11'h7C2:         begin  // LDUR
+                             reg2Loc      = 1'bX
+                             reg30        = 1'b0
+                             regWrite     = 1'b1
+                             ALUSrc       = 1'b1
+                             ALUOp        = 3'b010
+                             setFlags     = 1'b0
+                             memRead      = 1'b1
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'b1
+                             PC2Reg       = 1'b0
+                             branch       = 1'b0
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'b11
+                             end
             
-            12'h7C0:           // STUR
+            11'h7C0:         begin  // STUR
+                             reg2Loc      = 1'b1
+                             reg30        = 1'bX
+                             regWrite     = 1'b0
+                             ALUSrc       = 1'b1
+                             ALUOp        = 3'b010
+                             setFlags     = 1'b0
+                             memRead      = 1'bX
+                             memWrite     = 1'b1
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'b0
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'b11
+                             end
             
-            12'h758:           // SUBS
+            11'h758:         begin  // SUBS
+                             reg2Loc      = 1'b0
+                             reg30        = 1'b0
+                             regWrite     = 1'b1
+                             ALUSrc       = 1'b0
+                             ALUOp        = 3'b011
+                             setFlags     = 1'b1
+                             memRead      = 1'bX
+                             memWrite     = 1'b0
+                             mem2Reg      = 1'b0
+                             PC2Reg       = 1'b0
+                             branch       = 1'b0
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'b0
+                             reg2PC       = 1'b0
+                             extend       = 2'bX
+                             end
 
-            default: 
-
-
+            default:        begin 
+                             reg2Loc      = 1'bX
+                             reg30        = 1'bX
+                             regWrite     = 1'bX
+                             ALUSrc       = 1'bX
+                             ALUOp        = 1'bX
+                             setFlags     = 1'bX
+                             memRead      = 1'bX
+                             memWrite     = 1'bX
+                             mem2Reg      = 1'bX
+                             PC2Reg       = 1'bX
+                             branch       = 1'bX
+                             whichCMP     = 1'bX
+                             uncondBranch = 1'bX
+                             reg2PC       = 1'bX
+                             extend       = 1'bX
+                             end
         endcase
     end
 endmodule
